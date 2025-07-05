9631bf4949e78bb7c2254776591152da4fb060ca
OblivionSword <hunter140797@gmail.com>
2025-07-05 16:35:24 +0200

    added functionality for showing the ability description on mouse hover

---
+++ Assets/Script/UI/AbilityUIButton.cs
@@ -0,0 +1,27 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class AbilityUIButton : MonoBehaviour, IPointerEnterHandler
+{
+    [SerializeField] string description;
+    [SerializeField] AbilitySlotPositions slotPositions = new AbilitySlotPositions();
+    [SerializeField] int cost;
+    [SerializeField] TextMeshProUGUI descriptionBoxTextUI;
+
+    private void Awake()
+    {
+        descriptionBoxTextUI = GameObject.Find("DescriptionBoxText").GetComponent<TextMeshProUGUI>();
+    }
+
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        descriptionBoxTextUI.text = description + "\n" +
+                                    "\n" +
+                                    "Slot position: " + slotPositions + " Slot" +"\n" +
+                                    "\n" +
+                                    "Energy cost: " + cost;
+    }
+
+}
