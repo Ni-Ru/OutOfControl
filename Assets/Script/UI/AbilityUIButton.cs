41634a382fc4a3df4ce07b54e95f16d682dda855
OblivionSword <hunter140797@gmail.com>
2025-07-06 17:10:10 +0200

    added a UI navigation with controller functionality

--- Assets/Script/UI/AbilityUIButton.cs
+++ Assets/Script/UI/AbilityUIButton.cs
@@ -1,8 +1,9 @@
 using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
-public class AbilityUIButton : MonoBehaviour, IPointerEnterHandler
+public class AbilityUIButton : MonoBehaviour, IPointerEnterHandler, ISelectHandler
 {
     [SerializeField] string description;
     [SerializeField] AbilitySlotPositions slotPositions = new AbilitySlotPositions();
@@ -10,10 +11,44 @@ public class AbilityUIButton : MonoBehaviour, IPointerEnterHandler
     [SerializeField] string keyBindDescription;
     [SerializeField] TextMeshProUGUI descriptionBoxTextUI;
 
+    GameObject lastSelected;
+
+    public Button fallbackButton;
+
+
     private void Awake()
     {
         descriptionBoxTextUI = GameObject.Find("DescriptionBoxText").GetComponent<TextMeshProUGUI>();
     }
+
+    private void Update()
+    {
+        if (EventSystem.current.currentSelectedGameObject != null)
+            lastSelected = EventSystem.current.currentSelectedGameObject;
+
+        if (EventSystem.current.currentSelectedGameObject == null && lastSelected != null)
+            EventSystem.current.SetSelectedGameObject(lastSelected);
+    }
+
+    public void OnButtonClicked(Button currentButton)
+    {
+        
+        Button[] allButtons = Resources.FindObjectsOfTypeAll<Button>();
+
+        
+        foreach (Button btn in allButtons)
+        {
+            if (btn != currentButton && btn.gameObject.activeInHierarchy && btn.interactable)
+            {
+                EventSystem.current.SetSelectedGameObject(btn.gameObject);
+                break;
+            }
+        }
+
+        
+        currentButton.gameObject.SetActive(false);
+    }
+
 
 
     public void OnPointerEnter(PointerEventData eventData)
@@ -27,4 +62,14 @@ public class AbilityUIButton : MonoBehaviour, IPointerEnterHandler
                                     "Energy cost: " + cost;
     }
 
+    public void OnSelect(BaseEventData eventData)
+    {
+        descriptionBoxTextUI.text = description + "\n" +
+                                    "\n" +
+                                    "Slot position: " + slotPositions + " Slot" + "\n" +
+                                    "\n" +
+                                    "Keybind: " + keyBindDescription + "\n" +
+                                    "\n" +
+                                    "Energy cost: " + cost;
+    }
 }
